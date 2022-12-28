0667683bcdf83735cb62ca4bf9f883f773907e92
DmiTsa <you@example.com>
2022-12-28 23:28:19 +0300

    р1

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 // import express from 'express';
+const sequelize = require('./db');
 
-const PORT = 3500;
+const PORT = process.env.PORT || 3500;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(`Error! ${e}`);
+  }
+};
+start();
